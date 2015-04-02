d1db60106aa63c1d5c8ed8a64fa65a5fb3ca6911
Michael Pearlson <mapearlson@gmail.com>
2015-04-02 15:14:44 +0400

    arima graphs

--- forecast.R
+++ forecast.R
@@ -1,4 +1,19 @@
-setwd("~/Documents/Delma/Births")
+library(forecast)
+library(fpp)
+library(ggplot2)
+require(latticeExtra)
+require(ggplot2)
+require(reshape2)
+require(devtools)
+suppressPackageStartupMessages(
+  require(googleVis)
+)
+require(quantmod)
+require(PerformanceAnalytics)
+require(xtsExtra)
+require(devtools)
+install_github('rCharts', 'ramnathv')
+
 # Total and national fertility/crude birth rates are highly correlated.
 # Thus, I will use total birth rates given the increase in the amount of historical
 # data.
@@ -6,9 +21,9 @@ d <- read.csv("rate2.csv")
 str(d)
 dts <- ts(d$rate, start=c(1960, 1), end=c(2012, 1), frequency=1) 
 dts <- ts(d)
-plot(dts)
+plot(dts) # giant trend dowanward.  Have to difference.
 
-# simple exponential - models level
+# simple exponential 
 fitHW <- HoltWinters(dts, beta=FALSE, gamma=FALSE)
 # double exponential - models level and trend
 fitHW2 <- HoltWinters(dts, gamma=FALSE)
@@ -16,11 +31,9 @@ fitHW2.2 <- forecast.HoltWinters(fitHW2, h=7)
 plot.forecast(fitHW2.2)
 
 # predictive accuracy
-install.packages("forecast")
-library(forecast)
 accuracy(fitHW)
 
-# predict next three future values
+# predict next seven future values
 forecast(fitHW2, 7)
 plot(forecast(fitHW2, 7))
 
@@ -32,20 +45,21 @@ forecast(fitHW3, 7)
 
 acf(fitHW2.2$residuals, lag.max=20)
 Box.test(fitHW2.2$residuals, lag=20, type="Ljung-Box")
+# Box-Ljung test
+# data:  fitHW2.2$residuals
+# X-squared = 310.6668, df = 20, p-value < 2.2e-16
 
 plot.ts(fitHW2.2$residuals)
 plotForecastErrors(fitHW2.2$residuals)
 
-###ARIMA
+# Must use ARIMA.  Above steps are pretty unecessary, given that the plot is
+# obviously non-stationary, but I wanted to go through them.  
+# ARIMA
 acf(dts)
 pacf(dts)
 dts.diff <- diff(dts, differences=1)
 plot.ts(dts.diff)
 
-install.packages("fUnitRoots")
-library(fUnitRoots)
-install. packages("fpp")
-library(fpp)
 ############################
 pop.ts <- ts(d$total.pop, start=c(1960, 1), end=c(2012, 1), frequency=1) 
 
@@ -57,8 +71,7 @@ popHW2.2 <- forecast.HoltWinters(popHW2, h=7)
 plot.forecast(popHW2.2)
 
 # predictive accuracy
-install.packages("forecast")
-library(forecast)
+
 accuracy(fitHW)
 
 # predict next three future values
@@ -97,6 +110,7 @@ arima.forecast <- forecast.Arima(ddts.arima, h=10)
 arima.forecast
 summary(arima.forecast)
 plot(arima.forecast)
+accuracy(arima.forecast)
 
 acf(arima.forecast$residuals, lag.max=20)
 Box.test(arima.forecast$residuals, lag=20, type="Ljung-Box")
@@ -106,3 +120,20 @@ Box.test(arima.forecast$residuals, lag=20, type="Ljung-Box")
 
 plot.ts(arima.forecast$residuals) # make time plot of forecast errors
 plotForecastErrors(arima.forecast$residuals) # make a histogram
+
+fert.ts <- ts(d$fert, start=c(1960, 1), end=c(2012, 1), frequency=1)
+
+#graphs
+par(mfrow = c(3,1))
+plot(dts, ylab="Crude birth rate (1,000)") 
+plot(fert.ts, ylab="Fertility Rate")
+plot(pop.ts, ylab="Total Population")
+
+ggplot() + geom_line(data=dts, aes(x = time, y = "Crude Birth Rate", color = isin)) 
+stat_smooth(data=arima.forecast$, aes(x = time, y = M, color = isin))
+
+line <- transform(d, date = as.character(year))
+m1 <- mPlot(x = "year", y = "rate", type = "Line", data = d)
+m1$set(pointSize = 0, lineWidth = 1)
+m1$print("chart2")
+
