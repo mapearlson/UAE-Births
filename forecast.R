804a1ec0d32c25d37527f7ac5a6ee1b365afd90d
Michael Pearlson <mapearlson@gmail.com>
2015-04-07 17:48:05 +0400

    Data and pop

--- forecast.R
+++ forecast.R
@@ -134,7 +134,6 @@ stat_smooth(data=arima.forecast$, aes(x = time, y = M, color = isin))
 
 # although slightly out of chronology
 # I'll also use theEconomist from latticeExtra
-par(mfrow = c(3,1))
 asTheEconomist(
   xyplot(
     dts,
@@ -182,5 +181,55 @@ m1
 nat <- read.csv("national.csv")
 plot(nat$pop.n)
 
+#########################Fertility
+# ARIMA
+acf(pop.ts)
+pacf(pop.ts)
+dts.diff <- diff(dts, differences=1)
+plot.ts(dts.diff)
+
+############################
+pop.ts <- ts(d$total.pop, start=c(1960, 1), end=c(2012, 1), frequency=1) 
+
+
+
+acf(fitHW2.2$residuals, lag.max=20)
+Box.test(fitHW2.2$residuals, lag=20, type="Ljung-Box")
+
+plot.ts(fitHW2.2$residuals)
+plotForecastErrors(fitHW2.2$residuals)
+
+###ARIMA
+acf(dts)
+pacf(dts)
+
+plot(diff(log(dts)))
+ddts <- diff(log(dts))
+
+acf(ddts, lag.max=20)
+acf(ddts, lag.max=20, plot=FALSE)
+pacf(ddts, lag.max=20) # plot a partial correlogram
+pacf(ddts, lag.max=20, plot=FALSE) 
+auto.arima(dts)
+
+ddts.arima <- arima(dts, order=c(4,2,3))# fit an ARIMA(4,2,3) model
+ddts.arima2 <- arima(dts, order=c(2,1,2))
+
+arima.forecast <- forecast.Arima(ddts.arima, h=10)
+arima.forecast
+summary(arima.forecast)
+plot(arima.forecast)
+accuracy(arima.forecast)
+
+acf(arima.forecast$residuals, lag.max=20)
+Box.test(arima.forecast$residuals, lag=20, type="Ljung-Box")
+# Box-Ljung test
+# data:  arima.forecast$residuals
+# X-squared = 27.1241, df = 20, p-value = 0.1318
+
+plot.ts(arima.forecast$residuals) # make time plot of forecast errors
+plotForecastErrors(arima.forecast$residuals) # make a histogram
+
+fert.ts <- ts(d$fert, start=c(1960, 1), end=c(2012, 1), frequency=1)
 
 
